742c210e62eb81949cfc14e2d88cb2e34d8791b0
miho <miho_nagoya@totec.co.jp>
2026-06-19 09:19:38 +0900

    accounts-models.py_start(45)

--- photoproject/accounts/models.py
+++ photoproject/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# AbstractUserクラスをインポート
+from django.contrib.auth.models import AbstractUser
+
+class customUser(AbstractUser):
+    '''
+    Userモデルを継承したカスタムユーザーモデル
+    '''
+    pass
