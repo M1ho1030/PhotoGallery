e45b70c33e2fbedc01a906e7cc4c1d83c93d3665
miho <miho_nagoya@totec.co.jp>
2026-06-22 15:34:45 +0900

    photoproject-urls.py_ADD wordreset URLpatterns(95)

--- photoproject/photoproject/urls.py
+++ photoproject/photoproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# auth.viewsをインポートしてauth_viewという名前で利用する
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,30 @@ urlpatterns = [
 
     #accounts.urlsへのURLパターン
     path('', include('accounts.urls')),
+
+    # パスワードリセットのためのURLパターン
+    # PasswordResetConfirmViewがプロジェクトのUrls.pyを参照するのでここに記載。
+    # パスワードリセット申し込みのページ
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name = "password_reset.html"),
+         name ='password_reset'),
+
+    # メール送信完了ページ
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name = "password_reset_sent.html"),
+         name = 'password_reset_done'),
+    
+    # パスワードリセットページ
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name = "password_reset_form.html"),
+         name = 'password_reset_confirm'),
+
+    # パスワードリセット完了ページ
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name = "password_reset_done.html"),
+         name ='password_reset_complete'),
 ]
